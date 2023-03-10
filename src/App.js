13b8dcf25d423eb3840e0e8a5da4ea4455c4d327
Inseo Yang <y.1nseooo@gmail.com>
2023-03-10 15:57:16 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { __getTodos } from './redux/modules/todosSlice';
 
 function App() {
+
+  const dispatch = useDispatch();
+  const {isLoading, error, todos} = useSelector((state)=>{
+    return state.todos
+  })
+
+  useEffect(() => {
+    dispatch(__getTodos())
+  }, [])
+  
+
+  if (isLoading) {
+    return <div>로딩 중..</div>
+  }
+
+  if (error) {
+    <div>{error.message}</div>
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {todos.map((item)=>{
+        return <div key={item.id}>
+        {item.id}:{item.title}
+      </div>
+    })}
     </div>
   );
 }
